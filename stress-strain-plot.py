611925d6d604063e2d630b27b969f10d0f42d50f
Owen Strong <owensanstrong@gmail.com>
2025-10-02 11:44:08 -0500

    added option to clarify tensile vs compressive for true strain calculation

--- stress-strain-plot.py
+++ stress-strain-plot.py
@@ -3,6 +3,7 @@ import pandas as pd
 import argparse
 from io import StringIO
 import numpy as np
+from scipy.optimize import curve_fit
 
 ## Constants
 PI = 3.141592653589
@@ -15,6 +16,8 @@ parser.add_argument('-c', '--curve-threshold', help="The curvature/second deriva
 parser.add_argument('-t','--true-stress', help="If given, also plots the true stress (compared to engineering stress)", action='store_true')
 parser.add_argument('-m','--modulus-range', help="The range of the dataset (where [0:1] is full range) to average slope over to calculate elastic modulus")
 parser.add_argument('-r', '--range-estimation', help="If specified, graphs just the strain and stress of each vs timestamp, to get ranges for slope estimation" ,action='store_true')
+parser.add_argument('--tensile', help="If the strain is tensile rather than compressive, for calculating true strain", action='store_true' )
+parser.add_argument('-p', '--power-law', help="Plot a power law relationship (sig_t = K(eps_t)^n) for the given range assumed to be plastic. Include 'ng' to skip graphing, include 'nl' to graph in normal axes instead of log-log")
 
 args = parser.parse_args()
 
@@ -25,9 +28,22 @@ mat_mods:dict = {}
 mat_bs:dict = {} #Intercept offset to get it to line up with the straight region of the graph
 mat_yields:dict = {} #Yield Strengths of materials
 mat_ultimates:dict = {} #Ultimate Strengths of materials
+mat_k:dict = {} #K values of power law
+mat_n:dict = {} #n values of power law
 if not args.modulus_range is None:
     bounds = args.modulus_range.split("-")
     mod_range = (float(bounds[0]), float(bounds[1]))
+power_law = False
+graph_pl = False
+if not args.power_law is None:
+    power_law = True
+    if not 'ng' in args.power_law:
+        graph_pl = True
+        pl_loglog = True
+    if 'nl' in args.power_law:
+        pl_loglog = False
+    bounds = args.power_law.replace('nl', '').replace('ng','').split('-')
+    pl_range = (float(bounds[0]), float(bounds[1]))
 
 for fpath in fpaths:
     ## Get file text, and process the curve data out
@@ -79,10 +95,15 @@ for fpath in fpaths:
     
     graph_df.loc[:, 'Strain'] = strain_col
 
-    # Record true stress
+    # Record true stress and strain
     stress_col = graph_df['Stress']
     graph_df.loc[0, 'Stress (True)'] = stress_col[0]
-    graph_df.loc[1:, 'Stress (True)'] = stress_col[1:].astype(float) * (1 - strain_col[1:].astype(float))
+    if args.tensile:
+        graph_df.loc[1:, 'Stress (True)'] = stress_col[1:].astype(float) * (1 + strain_col[1:].astype(float))
+    else:
+        graph_df.loc[1:, 'Stress (True)'] = stress_col[1:].astype(float) * (1 - strain_col[1:].astype(float))
+    graph_df.loc[0, 'Strain (True)'] = strain_col[0]
+    graph_df.loc[1:, 'Strain (True)'] = np.log(strain_col[1:].astype(float) + 1)
 
     # Modulus calculation
     if not mod_range is None:
@@ -117,6 +138,21 @@ for fpath in fpaths:
 
         mat_ultimates[mat_name] = max(graph_df['Stress'][1:].astype(float))
         print(f"Ultimate Strength: {mat_ultimates[mat_name]} {stress_col[0]}")
+
+    # Power Law calculation
+    if power_law:
+        df = graph_df
+        true_strain = df['Strain (True)'][1:].astype(float)
+        true_stress = df['Stress (True)'][1:].astype(float)
+        min_idx = int(pl_range[0] * len(true_strain))
+        max_idx = int(pl_range[1] * len(true_strain))
+        tstrain_log = np.log(true_strain)
+        tstress_log = np.log(true_stress)
+        n, log_k = np.polyfit(tstrain_log[min_idx:max_idx], tstress_log[min_idx:max_idx], 1)
+        k = np.e**log_k
+        print(f"Power law: K={k}, n={n}")
+        mat_k[mat_name] = k
+        mat_n[mat_name] = n
 
     # Greatest strain value:
     print(f"Maxmimum strain: {max(strain_col[1:].astype(float))} {strain_col[0]}")
@@ -158,17 +194,32 @@ for mat_name in mat_dfs:
         strain = df['Strain'][1:].astype(float)
         stress = df['Stress'][1:].astype(float)
         ax.plot(strain, stress, label=mat_name+" (Eng)")
-        ax.plot(strain, df['Stress (True)'][1:].astype(float), label=mat_name+" (True)")
+        ax.plot(df['Strain (True)'][1:].astype(float), df['Stress (True)'][1:].astype(float), label=mat_name+" (True)")
         #num_idxs = len(strain)
         #min_idx = int(mod_range[0] * num_idxs)
         #max_idx = int(mod_range[1] * num_idxs)
         # ax.plot(df['Strain'][min_idx:max_idx].astype(float), df['Strain'][min_idx:max_idx].astype(float) * mat_mods[mat_name], label=mat_name+" Slope")
         if not (mod_range is None): ax.plot(strain, df['Offset'][1:].astype(float), label=mat_name+" 2% offset yiel")
         # ax.plot([df['Strain'][max_idx]]*2, [0, max(df['Strain'][1:].astype(float) * mat_mods[mat_name])])
+    elif graph_pl:
+        true_strain = df['Strain (True)'][1:].astype(float)
+        true_stress = df['Stress (True)'][1:].astype(float)
+        min_idx = int(pl_range[0] * len(true_strain))
+        max_idx = int(pl_range[1] * len(true_strain))
+        # ax.loglog(true_strain[min_idx:max_idx], true_stress[min_idx:max_idx], label=mat_name)
+        if pl_loglog:
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+        ax.plot(true_strain, true_stress, label=mat_name+" (True)")
+        k = mat_k[mat_name]
+        n = mat_n[mat_name]
+        ax.plot(true_strain, k*np.float_power(true_strain, n), label=mat_name+" (power law approx)")
+        ax.axis('tight')
     else:
         ax.plot(df['Strain'][1:].astype(float), df['Stress'][1:].astype(float), label=mat_name)
 
-ax.set_xticks(np.append(np.arange(min_str, max_str, label_freq*1.01), max_str))
+if not power_law:
+    ax.set_xticks(np.append(np.arange(min_str, max_str, label_freq*1.01), max_str))
 ax.set_xlabel("Strain (mm/mm)")
 ax.set_ylabel("Stress (kPa)")
 
